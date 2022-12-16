c0deff01ee3525c6d6eef2e73aed05460459da12
Manikandan Sundararajan <mani@hey.com>
2022-12-16 08:07:36 -0500

    template for each day

---
+++ src/template.rs
@@ -0,0 +1,23 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    error::Error,
+    fs::{read_to_string, File},
+    io::BufRead,
+    io::BufReader,
+};
+
+fn process_part1() -> Result<(), Box<dyn Error>> {
+    todo!();
+    Ok(())
+}
+
+fn process_part2() -> Result<(), Box<dyn Error>> {
+    todo!();
+    Ok(())
+}
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    process_part1()?;
+    process_part2()?;
+    Ok(())
+}
