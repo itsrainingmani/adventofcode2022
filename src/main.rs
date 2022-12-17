f8d163190fbc77d70f36b5c467ebf2aaa35a1729
Manikandan Sundararajan <mani@hey.com>
2022-12-17 00:18:37 -0500

    template

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 use std::error::Error;
 
 use adventofcode2022::{
-    day1, day10, day11, day12, day13, day14, day15, day2, day3, day4, day5, day6, day7, day7b,
-    day8, day9,
+    day1, day10, day11, day12, day13, day14, day15, day16, day17, day2, day3, day4, day5, day6,
+    day7, day7b, day8, day9,
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -23,7 +23,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     // day12::main()?;
     // day13::main()?;
     // day14::main()?;
-    day15::main()?;
+    // day15::main()?;
+    // day16::main()?;
+    day17::main()?;
+    // day18::main()?;
+    // day19::main()?;
+    // day20::main()?;
+    // day21::main()?;
+    // day22::main()?;
+    // day23::main()?;
+    // day24::main()?;
+    // day25::main()?;
 
     Ok(())
 }
--- src/template.rs
+++ src/template.rs
@@ -18,6 +18,6 @@ fn process_part2() -> Result<(), Box<dyn Error>> {
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     process_part1()?;
-    process_part2()?;
+    // process_part2()?;
     Ok(())
 }
